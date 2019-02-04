f749286a78cfe9fd72ec7d6edf18c704e1474a53
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-02-04 19:42:41 +0200

    start refactoring

--- labs/linked_list/src/main/java/node/DLNode.java
+++
@@ -1,7 +0,0 @@
-package node;
-
-public class DLNode {
-    public int data;
-    public DLNode next;
-    public DLNode prev;
-}
--- labs/linked_list/src/main/java/node/SLNode.java
+++
@@ -1,6 +0,0 @@
-package node;
-
-public class SLNode {
-    public int data;
-    public SLNode next;
-}
